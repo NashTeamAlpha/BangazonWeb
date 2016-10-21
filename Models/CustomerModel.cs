b12c2d43f34c2338b57b8f781fe515b6e77aada8
Chris Smalley <chrism.smalley@gmail.com>
2016-10-21 15:19:38 -0500

    Created CustomerModel

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Bangazon.Models
+{
+    public class Customer
+    {
+        [key]
+
+        public int CustomerId {get;set;}
+
+        [Required]
+
+        public string FirstName {get;set;}
+
+        [Required]
+
+        public string LastName {get;set;}
+
+        public ICollection<PaymentType> PaymentTypes;
+
+        public ICollection<Order> Orders;
+    }
+}

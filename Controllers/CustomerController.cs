7ee03baca54e255c4557ce41de4059069cef8f0c
Chris Smalley <chrism.smalley@gmail.com>
2016-10-24 10:50:04 -0500

    cs built new and overloaded new methods for customer controller, passing to zr.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWeb.Models;
+using BangazonWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangazonWeb.Controllers
+{
+    public class CustomerController : Controller
+    {
+    // Class Name: Customers
+    // Author: Chris Smalley
+    // Purpose of the class: The purpose of this class is to manage the methods that will produce the data and functionality needed for all of the views in the user interface related to customers.
+    // Methods in Class: New(), ShoppingCart(), Payment(), OrderCompleted() 
+        private BangazonWebContext context;
+        public CustomerController(BangazonWebContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult New()
+        {
+            //Method Name: New
+            //Purpose of the Method: Loads new customer form to the view, view wil be static.
+            //Arguments in Method: Need more clarification here
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryTokenAttribute]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            //Method Name: OverloadedNew
+            //Purpose of the Method: Will take an optional parameter of the type "customer." Takes form data then checks its validity. Post to customer table in database then redirects to home page.
+            //Arguments in Method: Need more clarification here
+            if (ModelState.IsValid)
+            {
+                context.Add(customer);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+    }
+}

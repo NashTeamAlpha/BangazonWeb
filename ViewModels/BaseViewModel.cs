c723f5be1b5a58a699ed6733a8e735e7799c4589
Jamie Duke <jamsduke@gmail.com>
2016-10-26 11:39:24 -0500

    touched ViewModels dir and created BaseViewModel

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,55 @@
+using System.Linq;
+using BangazonWeb.Data;
+using BangazonWeb.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BangazonWeb.ViewModels
+{
+  public class BaseViewModel
+  {
+    public IEnumerable<SelectListItem> ListOfCustomers { get; set; }
+    private BangazonWebContext context;
+    private ActiveCustomer singleton = ActiveCustomer.Instance;  // Not yet implemented
+    public Customer ChosenCustomer 
+    {
+      get
+      {
+        // Get the current value of the customer property of our singleton
+        Customer customer = singleton.Customer;
+
+        // If no customer has been chosen yet, it's value will be null
+        if (customer == null)
+        {
+          // Return fake customer for now
+          return new Customer () {
+            FirstName = "Create",
+            LastName = "Account"
+          };
+        }
+
+        // If there is a customer chosen, return it
+        return customer;
+      }
+      set
+      {
+        if (value != null)
+        {
+          singleton.Customer = value;
+        }
+      }
+    }
+    public BaseViewModel(BangazonWebContext ctx)
+    {
+        context = ctx;
+        this.ListOfCustomers = context.Customer
+            .OrderBy(l => l.LastName)
+            .AsEnumerable()
+            .Select(li => new SelectListItem { 
+                Text = $"{li.FirstName} {li.LastName}",
+                Value = li.CustomerId.ToString()
+            });
+    }
+    public BaseViewModel() { }
+  }
+}

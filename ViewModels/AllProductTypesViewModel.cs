8f1d2179b868c902c96623ae3c25ec3cb8446b24
Debbie Bourne <dblashrink@hotmail.com>
2016-10-27 10:36:47 -0500

    fix class name in documentation

--- ViewModels/AllProductTypesViewModel.cs
+++ ViewModels/AllProductTypesViewModel.cs
@@ -4,7 +4,7 @@ using BangazonWeb.Data;
 
 namespace BangazonWeb.ViewModels
 {
-    //Class Name: Products
+    //Class Name: AllProductTypesViewModel
     //Author: Debbie Bourne
     //Purpose of the class: The purpose of this class is to manage the property and method that will produce the data and functionality needed for the view of all of the product types
     //Constructor in Class: AllProductTypesViewModel() 

5b0097bb42f431d8b6a26ba922073e5846e2fec7
Debbie Bourne <dblashrink@hotmail.com>
2016-10-27 10:13:05 -0500

    added documentation of class and method

--- ViewModels/AllProductTypesViewModel.cs
+++ ViewModels/AllProductTypesViewModel.cs
@@ -6,7 +6,14 @@ namespace BangazonWeb.ViewModels
 {
     public class AllProductTypesViewModel : BaseViewModel
     {
+    //Class Name: Products
+    //Author: Debbie Bourne
+    //Purpose of the class: The purpose of this class is to manage the methods that will produce the data and functionality needed for all of the views in the user interface related to products.
+    //Methods in Class: AllProductTypesViewModel() 
         public IEnumerable<ProductType> ProductType { get; set; }
         public AllProductTypesViewModel(BangazonWebContext ctx) : base(ctx) { } 
+        //Method Name: Index
+        //Purpose of the Method: This method will return a list of all of the product types to the productsTypes view
+        //Arguments in Method: the BangazonWebContext is being passed into this argument
     }
 }

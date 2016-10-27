34a6a54b4e5735242201eeabebc796f1dd83f971
James Regnier <jgreg071@gmail.com>
2016-10-27 10:20:50 -0500

    Now have select item list of PaymentTypes named ListOfPaymentTypes

--- ViewModels/ShoppingCartViewModel.cs
+++ ViewModels/ShoppingCartViewModel.cs
@@ -1,13 +1,32 @@
 using System.Collections.Generic;
 using BangazonWeb.Models;
 using BangazonWeb.Data;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BangazonWeb.ViewModels
 {
   public class ShoppingCartViewModel : BaseViewModel
   {
+    public List<SelectListItem> ListOfPaymentTypes { get; set; }
     public IEnumerable<Product> Products { get; set; }
-    public IEnumerable<PaymentType> PaymentTypes { get; set; }
-    public ShoppingCartViewModel(BangazonWebContext ctx) : base(ctx) { }
+    private BangazonWebContext context;
+    private ActiveCustomer singleton = ActiveCustomer.Instance;
+
+    public ShoppingCartViewModel(BangazonWebContext ctx)
+    {
+        context = ctx;
+        this.ListOfPaymentTypes = context.PaymentType
+            .Where(pt => pt.CustomerId == singleton.Customer.CustomerId)
+            .AsEnumerable()
+            .Select(pt => new SelectListItem { 
+                Text = $"{pt.FirstName} {pt.LastName} {pt.Processor} {pt.ExpirationDate}",
+                Value = pt.PaymentTypeId.ToString()
+            }).ToList();
+        
+        this.ListOfPaymentTypes.Insert(0, new SelectListItem {
+          Text="Choose Payment Type"
+        });
+    }
   }
 }

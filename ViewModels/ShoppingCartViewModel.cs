606bec0ea369b97ce7fda7d0ddd1a14d2696fe71
James Regnier <jgreg071@gmail.com>
2016-10-27 10:30:38 -0500

    Added documentation to constructor method and class

--- ViewModels/ShoppingCartViewModel.cs
+++ ViewModels/ShoppingCartViewModel.cs
@@ -6,13 +6,20 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BangazonWeb.ViewModels
 {
+    //Class Name: ShoppingCartViewModel
+    //Author: Grant Regnier
+    //Purpose of the class: The purpose of this class is to pass data from the controller to the shoppingcart view with a selectlist of PaymentTypes
+    //Methods in Class: ShoppingCartViewModel(ctx)
   public class ShoppingCartViewModel : BaseViewModel
   {
     public List<SelectListItem> ListOfPaymentTypes { get; set; }
     public IEnumerable<Product> Products { get; set; }
     private BangazonWebContext context;
     private ActiveCustomer singleton = ActiveCustomer.Instance;
-
+    
+    //Method Name: ShoppingCartViewModel
+    //Purpose of the Method: Upon construction this should take the context and send a list of select items of the type PaymentType to the View. They should be the paymentTypes of the active customer.
+    //Arguments in Method: BangazonWebContext
     public ShoppingCartViewModel(BangazonWebContext ctx)
     {
         context = ctx;

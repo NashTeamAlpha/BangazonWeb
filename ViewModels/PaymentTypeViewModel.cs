b6df6e0a72e6f9610f71038d8e9008eb748b83be
James Regnier <jgreg071@gmail.com>
2016-10-28 13:31:10 -0500

    Update PaymentTypeViewModel.cs

--- ViewModels/PaymentTypeViewModel.cs
+++ ViewModels/PaymentTypeViewModel.cs
@@ -6,7 +6,7 @@ namespace BangazonWeb.ViewModels
     //Class Name: PAymentTypeViewModel
     //Author: Grant Regnier
     //Purpose of the class: The purpose of this class is to pass the type of PaymentType to the view for our payment form to model from.
-    //Constructor in Class: PAymentTypeViewModel()
+    //Constructor in Class: PaymentTypeViewModel()
     public class PaymentTypeViewModel : BaseViewModel
     {
         public PaymentType PaymentType { get; set; }

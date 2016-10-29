3afa46a289d16d9a271ca2b4e024e1443c7c2c0a
Jamie Duke <jamsduke@gmail.com>
2016-10-29 12:28:14 -0500

    updated documentation on view model

--- ViewModels/AllSubProductTypesViewModel.cs
+++ ViewModels/AllSubProductTypesViewModel.cs
@@ -6,8 +6,8 @@ namespace BangazonWeb.ViewModels
 {
     //Class Name: AllSubProductTypesViewModel
     //Author: Jamie Duke
-    //Purpose of the class: The purpose of this class is to manage the property and method that will produce the data and functionality needed for the view of all of the product types
-    //Constructor in Class: AllProductTypesViewModel() 
+    //Purpose of the class: The purpose of this class is to manage the property and method that will produce the data and functionality needed for the view of all of the sub product types
+    //Constructor in Class: AllSubProductTypesViewModel() 
     public class AllSubProductTypesViewModel : BaseViewModel
     {
         public IEnumerable<SubProductType> SubProductTypes { get; set; }
